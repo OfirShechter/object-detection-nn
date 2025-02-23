35631597594951a7aaf4f271fc58e7158b175519
OfirShechter <ofir1847@gmail.com>
2025-02-23 17:42:01 +0200

    base vgg16

--- backbone/vgg16.py
+++ backbone/vgg16.py
@@ -4,30 +4,26 @@ from torchvision import models
 from PIL import Image
 import requests
 import matplotlib.pyplot as plt
+import os
 
 # Load pre-trained VGG16 model
 model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
 model.eval()  # Set to evaluation mode
 
 # Define image transformation (resize, normalize, convert to tensor)
-transform = models.VGG16_Weights.DEFAULT.transforms
+transform = models.VGG16_Weights.DEFAULT.transforms()
 
-# Load a few sample images from URLs
-image_urls = [
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Push_van_cat.jpg/640px-Push_van_cat.jpg",  # Cat
-    "https://upload.wikimedia.org/wikipedia/commons/9/99/Golden_Retriever_medium-to-long_coat.jpg",  # Dog
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Big_Ben_Clock_Tower_-_London%2C_England_-_April_2009.jpg/640px-Big_Ben_Clock_Tower_-_London%2C_England_-_April_2009.jpg"  # Landmark
-]
+images_base_path = "backbone/images"
 
 # Download ImageNet class labels
 LABELS_URL = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
 labels = requests.get(LABELS_URL).text.splitlines()
 
 # Perform inference on each image
-for image_url in image_urls:
+for image_path in os.listdir(images_base_path):
+    print(image_path)
     # Load image
-    response = requests.get(image_url, stream=True)
-    img = Image.open(response.raw).convert("RGB")
+    img = Image.open(f"{images_base_path}/{image_path}")
 
     # Preprocess image
     input_tensor = transform(img).unsqueeze(0)  # Add batch dimension
