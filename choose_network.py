17d5199d0d4ac400504628a765eaedb758b5e66c
OfirShechter <ofir1847@gmail.com>
2025-02-23 16:37:10 +0200

    choose- VGG16

--- choose_network.py
+++ choose_network.py
@@ -4,11 +4,15 @@ id2='87654321'
 #split ids to digits and sum them
 sum1 = sum(int(digit) for digit in id1)
 sum2 = sum(int(digit) for digit in id2)
-total_sum = sum1 + sum2
+digit_sum = str(sum1 + sum2)
+print(f"total sum of digits is: {digit_sum}")
 
-print(f"total sum of digits in both ids is: {total_sum}")
-output_digit = sum(int(digit) for digit in str(total_sum))
+# repeated digit sum
+while len(digit_sum) > 1:
+    digit_sum = str(sum(int(digit) for digit in digit_sum))
+    print(f"total sum of digits is: {digit_sum}")
 
+output_digit = int(digit_sum)
 print(f"output digit is: {output_digit}. selected network is:")
 if output_digit <= 3:
     print("ResNet18")
@@ -18,3 +22,9 @@ elif output_digit <= 9:
     print("MobileNet V3")
 else:
     print("None of the above, something went wrong")
+    
+# total sum of digits is: 78
+# total sum of digits is: 15
+# total sum of digits is: 6
+# output digit is: 6. selected network is:
+# VGG16
