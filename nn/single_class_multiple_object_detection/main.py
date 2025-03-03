92f34883a2440d7f12f32b78d15e1ab05cd1a9c3
OfirShechter <ofir1847@gmail.com>
2025-03-03 17:55:49 +0200

    multiple start

--- nn/single_class_multiple_object_detection/main.py
+++ nn/single_class_multiple_object_detection/main.py
@@ -1,7 +1,7 @@
 #%%
 import os
 
-remote = False
+remote = True
 
 if remote:
     base_path = '/home/dcor/niskhizov/Rar/object-detection-nn/nn'
@@ -22,13 +22,13 @@ from nn.single_class_multiple_object_detection.utils.constants import image_size
 import matplotlib.pyplot as plt
 import cv2
 #%%
-video_path = f'{base_path}/single_class_multiple_object_detection/video/Biggest Small Dog Day at the farm!.mp4'
+video_path = f'{base_path}/single_class_multiple_object_detection/video/Dogs playing outside. GUARANTEED to make you smile. Most watched video. top. let your dog listen.mp4'
 frame_new_size = (image_size, image_size)
 video, frames = FrameHelpers.get_video_and_frames(video_path)
 #%%
-# model_path = f"{base_path}/YOLO_VGG16/degug_notebooks/vgg_f_modele32_vgg16_checkpoint.pth.tar"
-model_path = f"{base_path}/single_class_multiple_object_detection/saved_models/vgg_f_modele32_vgg16_checkpoint.pth.tar"
-object_class_detector = Object_Class_Detector(model_path, iou_threshold=0.2, threshold=0.6, display_frames=True)
+model_path = f"{base_path}/YOLO_VGG16/degug_notebooks/vgg_f_modele32_vgg16_checkpoint.pth.tar"
+# model_path = f"{base_path}/single_class_multiple_object_detection/saved_models/vgg_f_modele32_vgg16_checkpoint.pth.tar"
+object_class_detector = Object_Class_Detector(model_path, iou_threshold=0.2, threshold=0.6, display_frames=False)
 #%%
 # len(frames)
 # np.save(f'{base_path}/single_class_single_object_detection/frames.npy', frames)
@@ -48,7 +48,7 @@ while frame_index < len(frames):
 
 #%%
 # save result frames as pkl
-np.save(f'{base_path}/single_class_multiple_object_detection/result_frames.npy', result_frames)
+# np.save(f'{base_path}/single_class_multiple_object_detection/result_frames.npy', result_frames)
 
 #%%
 # load result frames from pkl
@@ -58,19 +58,20 @@ len(result_frames)
 #%%
 # write all frames to video
 frame_new_size = (image_size, image_size)
-output_path = f'{base_path}/dog_detection.avi'
+output_path = f'{base_path}/multiple_dog_detection-5dogs.avi'
 frame_rate = 30
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 writer = cv2.VideoWriter(output_path, fourcc, frame_rate, frame_new_size)
 
-for frame in result_frames:
+for i, frame in enumerate(result_frames):
     frame = (frame * 255).astype(np.uint8)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     writer.write(frame)
+    print(f"frame {i} was written")
 writer.release()
 # # %%
 # np.array(result_frames).shape
-# # %%
+# %%
 import matplotlib.pyplot as plt
 plt.imshow(result_frames[0])
 plt.show()
