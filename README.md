5c4972575503019f1b6eb1b5d35971ebaf95609d
Ofir Shechter <55805888+OfirShechter@users.noreply.github.com>
2025-03-08 13:22:48 +0200

    Feat: Part 3- same class, multiple object detection (#3)

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ YOLO_VGG16: based on- https://www.geeksforgeeks.org/yolov3-from-scratch-using-py
 
 one dog detection: https://www.youtube.com/watch?v=jH2d_4XJTkY&ab_channel=FOXSports
 multiple dog detection: https://www.youtube.com/watch?v=dBYw5we8LzE&ab_channel=TheFarm
+
+multiple airplane detection: https://www.youtube.com/watch?v=hn5evdP9yDg&ab_channel=AirshowsinJapanandtheworldbyTonkatsu298
--- nn/single_class_multiple_object_detection/main.py
+++ nn/single_class_multiple_object_detection/main.py
@@ -16,19 +16,20 @@ else:
 #%%
 import numpy as np
 
-from nn.single_class_single_object_detection.object_class_detector import Object_Class_Detector
+
+from nn.single_class_multiple_object_detection.object_class_detector import Object_Class_Detector
 from nn.utils.frames_helpers import FrameHelpers
-from nn.YOLO_VGG16.utils.constants import image_size
+from nn.single_class_multiple_object_detection.utils.constants import image_size
 import matplotlib.pyplot as plt
 import cv2
 #%%
-video_path = f'{base_path}/single_class_single_object_detection/video/Best of 2024 Masters Agility Championships from Westminster Kennel Club _ FOX Sports.mp4'
+video_path = f'{base_path}/single_class_multiple_object_detection/video/RIAT 2014 Frecce Tricolori Italian Air Force The Royal International Air Tattoo.mp4'
 frame_new_size = (image_size, image_size)
 video, frames = FrameHelpers.get_video_and_frames(video_path)
 #%%
-model_path = f"{base_path}/YOLO_VGG16/degug_notebooks/vgg_f_modele32_vgg16_checkpoint.pth.tar"
-# model_path = f"{base_path}/single_class_single_object_detection/saved_models/vgg_f_modele32_vgg16_checkpoint.pth.tar"
-object_class_detector = Object_Class_Detector(model_path)
+model_path = f"{base_path}/YOLO_VGG16/degug_notebooks/vgg_f_airplain_modele20_vgg16_checkpoint.pth.tar"
+# model_path = f"{base_path}/single_class_multiple_object_detection/saved_models/vgg_f_airplain_modele20_vgg16_checkpoint.pth.tar"
+object_class_detector = Object_Class_Detector(model_path, iou_threshold=0.1, threshold=0.5, display_frames=False)
 #%%
 # len(frames)
 # np.save(f'{base_path}/single_class_single_object_detection/frames.npy', frames)
@@ -38,7 +39,7 @@ len(frames)
 #%%
 batch_size = 16
 result_frames = []  # Buffer to store all processed frames
-frame_index = 0 # Current frame index
+frame_index = 0# Current frame index
 while frame_index < len(frames):
     processed_frames = object_class_detector.plot_marked_images(
         frames[frame_index:frame_index+batch_size].copy())
@@ -48,7 +49,7 @@ while frame_index < len(frames):
 
 #%%
 # save result frames as pkl
-np.save(f'{base_path}/single_class_single_object_detection/result_frames.npy', result_frames)
+# np.save(f'{base_path}/single_class_multiple_object_detection/result_frames.npy', result_frames)
 
 #%%
 # load result frames from pkl
@@ -58,22 +59,23 @@ len(result_frames)
 #%%
 # write all frames to video
 frame_new_size = (image_size, image_size)
-output_path = f'{base_path}/dog_detection.avi'
-frame_rate = 30
+output_path = f'{base_path}/multiple_airplane_detection.avi'
+frame_rate = 25
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 writer = cv2.VideoWriter(output_path, fourcc, frame_rate, frame_new_size)
 
-for frame in result_frames:
+for i, frame in enumerate(result_frames):
     frame = (frame * 255).astype(np.uint8)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     writer.write(frame)
+    print(f"frame {i} was written")
 writer.release()
 # # %%
 # np.array(result_frames).shape
-# # %%
-# import matplotlib.pyplot as plt
-# plt.imshow(result_frames[15])
-# plt.show()
+# %%
+import matplotlib.pyplot as plt
+plt.imshow(result_frames[0])
+plt.show()
 # # %%
 # np.max(result_frames[15].astype(np.uint8))
 # # %%
