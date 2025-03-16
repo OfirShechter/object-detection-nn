6cd0b15f98505dc3eb7c00bab0080ce822c5ce1a
OfirShechter <ofir1847@gmail.com>
2025-03-16 20:30:54 +0200

    try

--- nn/YOLO_VGG16_OBB/model/loss.py
+++ nn/YOLO_VGG16_OBB/model/loss.py
@@ -29,10 +29,10 @@ class YOLOLoss(nn.Module):
 		anchors = anchors.reshape(1, 3, 1, 1, 2) 
 		# Box prediction confidence 
 		box_preds = torch.cat([self.sigmoid(pred[..., 1:3]), 
-							torch.exp(pred[..., 3:5]) * anchors 
+							torch.exp(pred[..., 3:5]) * anchors, pred[..., 5]
 							],dim=-1) 
 		# Calculating intersection over union for prediction and target 
-		ious = iou(box_preds[obj], target[..., 1:5][obj]).detach() 
+		ious = iou(box_preds[obj], target[..., 1:6][obj]).detach() 
 		# Calculating Object loss 
 		object_loss = self.mse(self.sigmoid(pred[..., 0:1][obj]), 
 							ious * target[..., 0:1][obj]) 
@@ -43,13 +43,13 @@ class YOLOLoss(nn.Module):
 		# Target box coordinates 
 		target[..., 3:5] = torch.log(1e-6 + target[..., 3:5] / anchors) 
 		# Calculating box coordinate loss 
-		box_loss = self.mse(pred[..., 1:5][obj], 
-							target[..., 1:5][obj]) 
+		box_loss = self.mse(pred[..., 1:6][obj], 
+							target[..., 1:6][obj]) 
 
 		
 		# Claculating class loss 
-		class_loss = self.cross_entropy((pred[..., 5:][obj]), 
-								target[..., 5][obj].long()) 
+		class_loss = self.cross_entropy((pred[..., 6:][obj]), 
+								target[..., 6][obj].long()) 
 
 		# Calculate angle loss
 		# Total loss 
